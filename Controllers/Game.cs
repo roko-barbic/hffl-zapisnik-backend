01478586d0d0fefed5e175cf696ed2eba80e355f
roko-barbic <rokobarosk12345@gmail.com>
2023-09-19 18:14:13 +0200

    bux fix

--- Controllers/Game.cs
+++ Controllers/Game.cs
@@ -116,10 +116,10 @@ public class GameController : ControllerBase
                         game.Club_Home_Score+=6;
                     }
                     else if(eventDto.Type == 4){
-                        game.Club_Home_Score+=2;
+                        game.Club_Home_Score+=1;
                     }
                     else if(eventDto.Type == 5){
-                       game.Club_Home_Score+=4;
+                       game.Club_Home_Score+=2;
                     }
                     else if(eventDto.Type ==3){
                         game.Club_Away_Score+=6;
